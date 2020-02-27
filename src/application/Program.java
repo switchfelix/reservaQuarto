c9e86b3ef8241238ec3a4a8d8ee0e061268599bd
switchfelix <m.felixskt@hotmail.com>
2020-02-26 21:28:05 -0300

    bad salution

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,19 +16,31 @@ public class Program {
 		
 		System.out.println("Room number: ");
 		int number = sc.nextInt();
-		System.out.println("Check-in date (dd/mm/yyyy): ");
+		System.out.print("Check-in date (dd/mm/yyyy): ");
 		Date checkIn = sdf.parse(sc.next());
-		System.out.println("Check-out date (dd/mm/yyyy): ");
+		System.out.print("Check-out date (dd/mm/yyyy): ");
 		Date checkOut = sdf.parse(sc.next());
 		
 		if (!checkOut.after(checkIn)) {
 			System.out.println("Error in reservation: Check-out date must be after check-in date");
 		} else {
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
-			System.out.println(reservation);
+			System.out.println("Reservation :" +reservation);
+			System.out.println();
+			System.out.println("Enter date to update the reservation:");
+			System.out.print("Check-in date (dd/mm/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.print("Check-out date (dd/mm/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+
+			String erro = reservation.updateDates(checkIn, checkOut);
+			if (erro != null) {
+				System.out.println("Erro in reservation " + erro);
+			}else {	
+			System.out.println("Reservation :" +reservation);
+		
+			}
 		}
-		
-		
 		sc.close();
 	}
 
